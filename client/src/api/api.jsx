ed1f2e945e22828da3675e85ae88a318e702d6c0
Nhut Linh <linhhn1210@gmail.com>
2024-05-14 10:17:52 +0700

    config axios

---
+++ client/src/api/api.jsx
@@ -0,0 +1,35 @@
+import axiosClient from "./axiosClient";
+
+export const api = {
+  //GET
+  getAStudentInfo: (ID, params) => {
+    const url = "student/" + String(ID);
+    return axiosClient.get(url, params);
+  },
+  getStudentInfoArr: (params) => {
+    const url = "student";
+    return axiosClient.get(url, params);
+  },
+  getTest: (params) => {
+    const url = "";
+    return axiosClient.get(url, params);
+  },
+
+  //POST******************************************************
+  postStudentInfo: (payload) => {
+    const url = "student";
+    return axiosClient.post(url, payload);
+  },
+
+  //PUT***************************
+  putSubject: (id, payload) => {
+    const url = "subject/" + String(id);
+    return axiosClient.put(url, payload);
+  },
+
+  //DELETE*****************************
+  deleteSubject: (id) => {
+    const url = "subject/" + String(id);
+    return axiosClient.delete(url);
+  },
+};
